99dc886631107b0dac5bbd2c11d63ea04730866d
mat701 <matteobruno180@gmail.com>
2025-11-15 11:29:32 +0100

    Adding parks

---
+++ locations.js
@@ -0,0 +1,72 @@
+const locations = [
+  {
+    "name": "Rome",
+    "coords": [
+      12.4964, 
+      41.9028
+    ],
+    "zoom": 11.8,
+    "pitch": 45,
+    "bearing": 0
+  },
+  {
+    "name": "Villa Borghese",
+    "coords": [
+      12.484466479963453,
+      41.9113
+    ],
+    "zoom": 15.4,
+    "pitch": 45,
+    "bearing": 0
+  },
+  {
+    "name": "Villa Doria Pamphilj",
+    "coords": [
+      12.44239218066872,
+      41.885232294343496
+    ],
+    "zoom": 15,
+    "pitch": 60,
+    "bearing": 0
+  },
+  {
+    "name": "Riserva Naturale di Monte Mario",
+    "coords": [
+      12.448,
+      41.925
+    ],
+    "zoom": 14,
+    "pitch": 60,
+    "bearing": -30
+  },
+  {
+    "name": "Villa Ada",
+    "coords": [
+      12.501260127292028,
+      41.9316
+    ],
+    "zoom": 15.2,
+    "pitch": 45,
+    "bearing": -15
+  },
+  {
+    "name": "Parco degli Acquedotti",
+    "coords": [
+      12.56271048950454,
+      41.84459765016617
+    ],
+    "zoom": 15,
+    "pitch": 60,
+    "bearing": 15
+  },
+  {
+    "name": "Parco della Caffarella",
+    "coords": [
+      12.51876222673077,
+      41.86326022220375
+    ],
+    "zoom": 15,
+    "pitch": 60,
+    "bearing": 20
+  }
+];
